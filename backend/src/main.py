e02b3d484d4d7a43721dc70cfb3c0b7a5ac5a418
Fernando <fdofuentescarrasco@gmail.com>
2025-08-08 16:45:11 +0200

    Adding router

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 import os
+from contextlib import asynccontextmanager
+from api.db import init_db
+from api.chat.routing import router as chat_router
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+app.include_router(chat_router, prefix="/api/chat")
 
 API_KEY= os.environ.get("API_KEY")
 
