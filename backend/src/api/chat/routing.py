ddae9e2bf2426722dfa75399c272371c2886369f
Fernando <fdofuentescarrasco@gmail.com>
2025-08-11 11:05:17 +0200

    Agent endpoint

--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -1,12 +1,17 @@
+import uuid
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
-from .models import ChatMessagePayload, ChatMessage, ChatMessageListItem
-from api.ai.services import generate_email_message
+from langgraph.checkpoint.memory import InMemorySaver
+
 from api.db import get_session
-from api.ai.schemas import EmailMessageSchema
+from api.ai.agents import get_supervisor
+from api.ai.schemas import EmailMessageSchema, SupervisorMessageSchema
+from api.ai.services import generate_email_message
+from .models import ChatMessagePayload, ChatMessage, ChatMessageListItem
 
 router= APIRouter()
+checkpointer = InMemorySaver()
 
 
 @router.get("/")
@@ -22,14 +27,30 @@ def chat_list_messages(session: Session = Depends(get_session)):
 
 
 
-@router.post("/", response_model=EmailMessageSchema)
-def chat_create_message(payload: ChatMessagePayload, session: Session = Depends(get_session)):
-    data= payload.model_dump()
-    print(data)
-    obj_instance = ChatMessage.model_validate(data)
-    session.add(obj_instance)
+@router.post("/", response_model=SupervisorMessageSchema)
+def chat_create_message(
+    payload:ChatMessagePayload,
+    session: Session = Depends(get_session)
+    ):
+    data = payload.model_dump() # pydantic -> dict
+    obj = ChatMessage.model_validate(data)
+    session.add(obj)
     session.commit()
-    #session.refresh(obj_instance)
+    thread_id = uuid.uuid4()
+    supe = get_supervisor(checkpointer=checkpointer)
+    msg_data = {
+        "messages": [
+            {"role": "user",
+            "content": f"{payload.message}" 
+          },
+        ]
+    }
+    result = supe.invoke(msg_data, {"configurable": {"thread_id": thread_id}})
+    if not result:
+        raise HTTPException(status_code=400, detail="Error with supervisor")
+    messages = result.get("messages")
+    if not messages:
+        raise HTTPException(status_code=400, detail="Error with supervisor")
+    return messages[-1]
 
-    response = generate_email_message(payload.message)
     return response
